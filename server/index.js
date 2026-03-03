465138a15bb0a16fe25e573b9b6e9d9cdbca802d
AyaazQureshi <ayaazq786@gmail.com>
2026-03-04 02:28:43 +0530

    Fixed CORS for production

--- server/index.js
+++ server/index.js
@@ -21,11 +21,13 @@ database.connect();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
+
+
 app.use(
-	cors({
-		origin:"http://localhost:3000",
-		credentials:true,
-	})
+  cors({
+    origin: "https://studynotion-done.vercel.app",
+    credentials: true,
+  })
 )
 
 app.use(
